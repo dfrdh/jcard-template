e6f0c07207ca1770e46df2f9846acc7caf7b4c81
Blaine Murphy <myself@blaines.world>
2018-01-02 22:27:19 -0500

    Reorganized fields to group related settings

--- static/scripts/jcard.js
+++ static/scripts/jcard.js
@@ -7,24 +7,27 @@ var jcard = (function() {
         var prefix = '#' + controls.id + '-';
         return {
             print2:     document.querySelector(prefix + 'print-2'),
-
-            cardColor:  document.querySelector(prefix + 'card-color'),
-            textColor:  document.querySelector(prefix + 'text-color'),
-            titleSize:  document.querySelector(prefix + 'title-size'),
-            typeSize:   document.querySelector(prefix + 'type-size'),
-            noteSize:   document.querySelector(prefix + 'note-size'),
-            trackSize:  document.querySelector(prefix + 'track-size'),
-            sideSize:   document.querySelector(prefix + 'side-size'),
             shortBack:  document.querySelector(prefix + 'short-back'),
 
             cover:      document.querySelector(prefix + 'cover'),
+            cardColor:  document.querySelector(prefix + 'card-color'),
+            textColor:  document.querySelector(prefix + 'text-color'),
+
             title:      document.querySelector(prefix + 'title'),
             subtitle:   document.querySelector(prefix + 'subtitle'),
+            titleSize:  document.querySelector(prefix + 'title-size'),
+
             type:       document.querySelector(prefix + 'type'),
+            typeSize:   document.querySelector(prefix + 'type-size'),
+
             noteUpper:  document.querySelector(prefix + 'note-upper'),
             noteLower:  document.querySelector(prefix + 'note-lower'),
+            noteSize:   document.querySelector(prefix + 'note-size'),
+
             sideA:      document.querySelector(prefix + 'side-a'),
-            sideB:      document.querySelector(prefix + 'side-b')
+            sideB:      document.querySelector(prefix + 'side-b'),
+            trackSize:  document.querySelector(prefix + 'track-size'),
+            backSize:   document.querySelector(prefix + 'back-size')
         }
     }
 
@@ -54,62 +57,66 @@ var jcard = (function() {
         }
     }
 
+    // add listeners to inputs that toggle option classes
+    function addOptionListeners(inputs, root) {
+        addToggleListener(inputs.print2, root, 'print-2');
+    }
+
     // add listeners to inputs that update j-card outputs
     function addJCardListeners(inputs, outputs) {
-        // layout
-        addColorListener(inputs.textColor, outputs.root, 'color');
-        addColorListener(inputs.cardColor, outputs.boundaries, 'backgroundColor');
-        outputs.titleGroups.forEach(function(groupOutput) {
-            addSizeListener(inputs.titleSize, groupOutput);
-        });
-        addSizeListener(inputs.typeSize, outputs.type);
-        addSizeListener(inputs.noteSize, outputs.noteGroup);
-        addSizeListener(inputs.trackSize, outputs.tracks);
-        addSizeListener(inputs.sideSize, outputs.sideA);
-        addSizeListener(inputs.sideSize, outputs.sideB);
         addToggleListener(inputs.shortBack, outputs.root, 'short-back');
 
-        // content
         addImageListener(inputs.cover, outputs.cover);
+        addColorListener(inputs.textColor, outputs.root, 'color');
+        addColorListener(inputs.cardColor, outputs.boundaries, 'backgroundColor');
+
         outputs.titles.forEach(function(titleOutput) {
             addTextListener(inputs.title, titleOutput);
         });
         outputs.subtitles.forEach(function(subtitleOutput) {
             addTextListener(inputs.subtitle, subtitleOutput);
         });
+        outputs.titleGroups.forEach(function(groupOutput) {
+            addSizeListener(inputs.titleSize, groupOutput);
+        });
+
         addTextListener(inputs.type, outputs.type);
+        addSizeListener(inputs.typeSize, outputs.type);
+
         addTextListener(inputs.noteUpper, outputs.noteUpper);
         addTextListener(inputs.noteLower, outputs.noteLower);
+        addSizeListener(inputs.noteSize, outputs.noteGroup);
+
         addSideListener(inputs.sideA, outputs.sideA);
         addSideListener(inputs.sideB, outputs.sideB);
         addTracksListener([inputs.sideA, inputs.sideB], outputs.tracks);
-    }
-
-    // add listeners to inputs that toggle option classes
-    function addOptionListeners(inputs, root) {
-        addToggleListener(inputs.print2, root, 'print-2');
+        addSizeListener(inputs.trackSize, outputs.tracks);
+        addSizeListener(inputs.backSize, outputs.sideA);
+        addSizeListener(inputs.backSize, outputs.sideB);
     }
 
     // populate inputs with field values or defaults
     function populate(inputs, fields) {
-        // layout
-        inputs.cardColor.value = fields.card_color || 'white';
-        inputs.textColor.value = fields.text_color || 'black';
-        inputs.titleSize.value = fields.title_size || 12;
-        inputs.typeSize.value = fields.type_size || 10;
-        inputs.noteSize.value = fields.note_size || 10;
-        inputs.trackSize.value = fields.track_size || 9;
-        inputs.sideSize.value = fields.side_size || 8;
         inputs.shortBack.checked = fields.short_back !== undefined ? fields.short_back : false;
 
-        // content
+        inputs.cardColor.value = fields.card_color || 'white';
+        inputs.textColor.value = fields.text_color || 'black';
+
         inputs.title.value = fields.title || '';
         inputs.subtitle.value = fields.subtitle || '';
+        inputs.titleSize.value = fields.title_size || 12;
+
         inputs.type.value = fields.type || '';
+        inputs.typeSize.value = fields.type_size || 10;
+
         inputs.noteUpper.value = fields.note_upper || '';
         inputs.noteLower.value = fields.note_lower || '';
+        inputs.noteSize.value = fields.note_size || 10;
+
         inputs.sideA.value = formatList(fields.side_a || []);
         inputs.sideB.value = formatList(fields.side_b || []);
+        inputs.trackSize.value = fields.track_size || 9;
+        inputs.backSize.value = fields.back_size || 8;
     }
 
     // trigger listener calls on all fields
